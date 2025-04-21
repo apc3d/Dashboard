039b1df008cea7ecaf3d7221d9841d82aa356085
apc3d <jimmy.lombard@apc3d.fr>
2025-04-21 14:16:24 +0200

    Delete dashboard.js

--- dashboard.js
+++
@@ -1,8 +0,0 @@
-dashboard.jfunction showTab(tabId) {
-  document.querySelectorAll('.tab').forEach(tab => tab.classList.remove('active'));
-  document.querySelectorAll('.menu li').forEach(item => item.classList.remove('active'));
-
-  document.getElementById(tabId).classList.add('active');
-  document.querySelector(`.menu li[onclick="showTab('${tabId}')"]`).classList.add('active');
-}
-s
